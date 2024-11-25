0224c4c85d8892013ff9d6ee5caf53b3e25b7622
jurij srebrnic <jurij.srebrnic@gmail.com>
2024-11-25 12:56:14 +0100

    Store the ssh pub key locally, renamed varaibles

--- terraform/key_pair/main.tf
+++ terraform/key_pair/main.tf
@@ -3,12 +3,17 @@ resource "tls_private_key" "rsa" {
   rsa_bits  = 4096
 }
 
-resource "aws_key_pair" "key" {
+resource "aws_key_pair" "ec2_pub_key" {
   key_name   = var.key_pair_name
   public_key = tls_private_key.rsa.public_key_openssh
 }
 
-resource "local_file" "local_key" {
+resource "local_file" "private_ssh_key" {
   content  = tls_private_key.rsa.private_key_pem
   filename = "${var.key_pair_name}.id_rsa"
+}
+
+resource "local_file" "public_ssh_key" {
+  content  = tls_private_key.rsa.public_key_openssh
+  filename = "${var.key_pair_name}.id_rsa.pub"
 }
