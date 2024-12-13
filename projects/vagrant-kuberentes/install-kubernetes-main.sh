bc4901674a44885d5ccb10d2834df46a00aeab2b
jurij srebrnic <jurij.srebrnic@gmail.com>
2024-12-13 15:12:55 +0100

    Removed comment

--- projects/vagrant-kuberentes/install-kubernetes-main.sh
+++ projects/vagrant-kuberentes/install-kubernetes-main.sh
@@ -5,8 +5,6 @@ SCRIPT_DIR=$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &>/dev/null && pwd)
 SSH_KEY=~/.vagrant.d/insecure_private_key
 ANSIBLE_TAG=${1:-}
 
-# setup grafana
-cli_command=(ansible-playbook --private-key="${SSH_KEY}" -i hosts -u vagrant --extra-vars "variable_host=kubernetes-main" "${SCRIPT_DIR}/../../ansible/kubernetes-main.yml")
 if [ -n "${ANSIBLE_TAG}" ]; then
     cli_command+=(--tags "${ANSIBLE_TAG}")
 fi
